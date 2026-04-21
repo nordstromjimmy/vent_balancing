8681eabbc05507fae4bdfb662134658a7a3fa241
nordstromjimmy <nordstromjimmy@gmail.com>
2026-04-21 19:09:22 +0200

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vent_balancing"
+    namespace = "com.jimmysvent.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
